a19566c7f684b2ae5f6762123f3bd83e9dad3c0b
gkuz <kuznetsovge@yandex.ru>
2019-01-19 19:25:18 +0500

    chapter 0: add meal models

---
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.topjava.model;
+
+import java.time.LocalDateTime;
+
+public class MealWithExceed {
+    protected final LocalDateTime dateTime;
+
+    protected final String description;
+
+    protected final int calories;
+
+    protected final boolean exceed;
+
+    public MealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.exceed = exceed;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExceed() {
+        return exceed;
+    }
+}
